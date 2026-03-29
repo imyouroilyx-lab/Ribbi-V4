84ccad1e2c23831e0f085d37de6ff2a87cbb935e
imyouroilyx-lab <imyouroilyx@gmail.com>
2026-03-30 04:32:20 +0700

    Update page.tsx

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,12 +1,16 @@
 'use client';
 
+import { Suspense } from 'react';
 import NavLayout from '@/components/NavLayout';
 import MessagesPage from '@/components/MessagesPage';
 
 export default function Messages() {
   return (
     <NavLayout>
-      <MessagesPage />
+      {/* ใส่ Suspense ครอบ Component ที่มีการเรียกใช้ useSearchParams() ไว้ด้านใน */}
+      <Suspense fallback={<div>กำลังโหลด...</div>}>
+        <MessagesPage />
+      </Suspense>
     </NavLayout>
   );
 }
